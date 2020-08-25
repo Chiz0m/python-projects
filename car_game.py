113fc52b7e6b989eae2233df32e6e9417cccaf47
Chizom Echehieuka <chizomreal@gmail.com>
2020-08-25 11:14:10 +0100

    this is a simple car game. You can start and stop the car lol

--- car_game.py
+++ car_game.py
@@ -2,17 +2,31 @@
 
 print('type help to see options')
 command = ""
-while command != 'quit':
-    command = input('>')
+car_started = False
+while True:
+    command = input('> ')
     command = command.lower()
     if command == 'quit':
-        break
+        if car_started:
+            print('Please type "stop" to stop the car')
+        else:
+            break
     elif command == 'start':
-        print('Car started')
-        command = ""
+        if car_started:
+            print('Car was already started')
+        else:
+            print('Car started')
+            command = ""
+            car_started = True
     elif command == 'stop':
-        print('Car stopped')
-        command = ""
+        if car_started:
+            print('Car stopped')
+            command = ""
+            car_started = False
+        elif car_started == False:
+            print("You already stopped the car initially")
+        else:
+            print("You can't turn off a car that is not started")
     elif command == 'help':
         print('start - to start car')
         print('stop - to stop car')
