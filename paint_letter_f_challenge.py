9ca55651c886d002609a91c04141bf0b30939fdc
Chizom Echehieuka <chizomreal@gmail.com>
2020-08-26 18:38:09 +0100

    this app prints an F and L on the screen

---
+++ paint_letter_f_challenge.py
@@ -0,0 +1,20 @@
+# this app paints the letter F in the terminal
+numbers = [5, 2, 5, 2, 2]
+
+for n in numbers:
+    output = ''
+    for m in range(n):
+        output += 'x'
+    print(output)
+
+print('')
+print('')
+print('')
+# this app paints the letter L in the terminal
+numbers = [2, 2, 2, 2, 5]
+
+for n in numbers:
+    output = ''
+    for m in range(n):
+        output += 'x'
+    print(output)
